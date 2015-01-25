0ddb94a3f86e8a85511071cbffa28943d4e8d1c9
Duy Tran <dangduy2910@gmail.com>
2015-01-26 00:25:27 +0700

    Integrate ngResource

--- src/app/app.js
+++ src/app/app.js
@@ -1,10 +1,11 @@
 angular.module('ngBoilerplate', [
-  'templates-app',
-  'templates-common',
-  'ngBoilerplate.home',
-  'ngBoilerplate.about',
-  'ui.router',
-  'ui.bootstrap'
+    'ngResource',
+    'templates-app',
+    'templates-common',
+    'ngBoilerplate.home',
+    'ngBoilerplate.about',
+    'ui.router',
+    'ui.bootstrap'
 ])
 
 .config(function myAppConfig($stateProvider, $urlRouterProvider) {
@@ -13,12 +14,18 @@ angular.module('ngBoilerplate', [
 
 .run(function run() {})
 
-.controller('AppCtrl', function AppCtrl($scope, $location) {
+.controller('AppCtrl', function AppCtrl($scope, $location, $resource) {
     $scope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
         if (angular.isDefined(toState.data)) {
             $scope.pageTitle = toState.data.pageTitle + ' | ngBoilerplate';
         }
     });
+    
+    var Theme = $resource('http://localhost:8080/sample/themes/');
+    var theme = Theme.save({}, function (a,b,c,d,e,f) {
+        console.log(JSON.stringify(theme));
+    });
+    
 })
 
 ;
